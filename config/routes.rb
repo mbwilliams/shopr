0547556f3e7888584e0994d3c7e88e3025ef193c
Marlon <marlon.willi@gmail.com>
2014-05-02 20:04:52 -0500

    [WIP] Shopping lists management

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
   resources :stores
+  resources :shopping_lists do
+    resources :items
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
