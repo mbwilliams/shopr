df7d188492cd8695b5df3153f7822b3f069c50d2
Marlon <marlon.willi@gmail.com>
2014-05-16 21:32:57 -0500

    Add unit test for Store model validations

--- test/models/store_test.rb
+++ test/models/store_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class StoreTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should not save store without name" do
+    store = Store.new
+    store.location = "My store's location"
+
+    assert_not store.save
+  end
+
+  test "should not save store without location" do
+    store = Store.new
+    store.name = "My Store"
+
+    assert_not store.save
+  end
+
 end
