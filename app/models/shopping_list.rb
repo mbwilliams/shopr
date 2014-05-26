f66735eda0e4fb7bf4bbf12655d9496c9eea2e0b
Marlon <marlon.willi@gmail.com>
2014-05-25 19:52:57 -0500

    Display store name on shopping list index

--- app/models/shopping_list.rb
+++ app/models/shopping_list.rb
@@ -2,4 +2,5 @@ class ShoppingList < ActiveRecord::Base
   belongs_to :store
 
   validates :purchase_date, presence: true
+  validates :store_id,      presence: true
 end
--- test/models/shopping_list_test.rb
+++ test/models/shopping_list_test.rb
@@ -9,4 +9,11 @@ class ShoppingListTest < ActiveSupport::TestCase
     assert_not shopping_list.save
   end
 
+  test "should not save shopping list without store" do
+    shopping_list = shopping_lists(:at_minimart)
+    shopping_list.store = nil
+
+    assert_not shopping_list.save
+  end
+
 end
